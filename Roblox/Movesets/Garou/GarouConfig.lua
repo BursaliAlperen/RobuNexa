24309945b022d72248d6c3e8bafd6520a3f1725c
BursaliAlperen <bursali.alperen.131@gmail.com>
2026-09-21 18:56:50 +0300

    Add Garou moveset folder structure

---
+++ Roblox/Movesets/Garou/GarouConfig.lua
@@ -0,0 +1,20 @@
+--!strict
+-- Generic Garou moveset metadata.
+return {
+	Id = "Garou",
+	DisplayName = "Garou",
+	Title = "Hero Hunter",
+	Rarity = "Mythic",
+	Price = 12000,
+	Currency = "Gems",
+	Enabled = true,
+
+	Abilities = {
+		"Flowing Water",
+		"Whirlwind Iron Cutting",
+		"Cross Fang Dragon Slayer",
+		"Monster Calamity",
+	},
+
+	AssetFolder = "Garou",
+}
