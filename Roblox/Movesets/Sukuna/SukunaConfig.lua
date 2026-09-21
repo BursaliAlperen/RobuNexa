81ac65f7ae64badb4ee3b6dd94f4cc80a14e8308
BursaliAlperen <bursali.alperen.131@gmail.com>
2026-09-21 18:56:43 +0300

    Add Sukuna moveset folder structure

---
+++ Roblox/Movesets/Sukuna/SukunaConfig.lua
@@ -0,0 +1,20 @@
+--!strict
+-- Generic Sukuna moveset metadata.
+return {
+	Id = "Sukuna",
+	DisplayName = "Ryomen Sukuna",
+	Title = "King of Curses",
+	Rarity = "Mythic",
+	Price = 10000,
+	Currency = "Gems",
+	Enabled = true,
+
+	Abilities = {
+		"Dismantle",
+		"Cleave",
+		"Fire Arrow",
+		"Malevolent Shrine",
+	},
+
+	AssetFolder = "Sukuna",
+}
